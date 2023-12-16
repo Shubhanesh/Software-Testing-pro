2b83c61e50b49194ce8fd321d2fb9bd4dc18680e
Shubhanesh <shubhanesh1994@gmail.com>
2023-12-16 21:28:01 +0530

    first commit

---
+++ src/Rel_Operator.java
@@ -0,0 +1,21 @@
+package src;
+
+public class Rel_Operator {
+    public static void main(String[] args) {
+        // Relational operator
+
+        int age_ganesh=30;
+        int age_shubha=29;
+        Boolean result= age_ganesh<age_shubha;
+        System.out.println(result);
+        // >, < >=,<=, == , != ( ! = )
+        int a=10;
+        int b=11;
+        boolean r1= a>=b;
+        System.out.println(r1);
+        boolean r2= a<=b;
+        System.out.println(r2);
+
+
+    }
+}
