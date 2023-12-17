24d6259cc7799d5caa3c3b6ac70e8e547e5e11ca
Shubhanesh <shubhanesh1994@gmail.com>
2023-12-17 07:37:25 +0530

    first commit

---
+++ src/Logic_Operator.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Logic_Operator {
+    public static void main(String[] args) {
+        //Logical Operator
+        // !, && m ||
+        // ! -> Not , with boolean or condition
+        boolean a=true;
+        System.out.println(a);
+        System.out.println(!a);
+        System.out.println(!(10>20));
+    }
+}
